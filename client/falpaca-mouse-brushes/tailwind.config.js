20f1c9fb8eae44fcab697320aa432648f66c180a
Pyrite32 <patrick@method93.com>
2025-04-17 12:40:49 -0700

    chore(client): add inter

--- client/falpaca-mouse-brushes/tailwind.config.js
+++ client/falpaca-mouse-brushes/tailwind.config.js
@@ -1,8 +1,19 @@
+
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // App directory
+    "./pages/**/*.{js,ts,jsx,tsx}", // Page directory
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter'],
+        barlow: ['Barlow Condensed'],
+      },
+    }
   },
   variants: {
     extend: {},
