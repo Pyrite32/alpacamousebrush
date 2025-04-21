fbc3b1ab9eb285b91fa5841aa93935e68d70dd03
Pyrite32 <patrick@method93.com>
2025-04-21 10:57:39 -0700

    build(client): declaring theme fonts not necessary

--- clientside/tailwind.config.js
+++ clientside/tailwind.config.js
@@ -9,12 +9,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-        barlow: ['Barlow', ...defaultTheme.fontFamily.serif],
-      },
-    }
+    
   },
   variants: {
     extend: {},
