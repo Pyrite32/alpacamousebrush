b9ecc9128fb69d2badc4e977d70119b058432b51
Pyrite32 <patrick@method93.com>
2025-04-21 11:54:14 -0700

    refactor(client): separate colors from theme

---
+++ clientside/src/priscilla/_colors/defaultColors.ts
@@ -0,0 +1,23 @@
+import { PriscillaThemeColors } from "./ts/PriscillaThemeColors";
+
+const defaultColors: PriscillaThemeColors = {
+    primary: "#D7BDCC",
+    secondary: "#9C7E9D",
+    tertiary: "#625760",
+    disabled: "#222028",
+    background: "#0D0E0F",
+    containerBackground: "#222028",
+    containerRim: "#625760",
+    accentSelected: "#F2257E",
+    accentSelectedBackdrop: "#49416166",
+    accentInfo: "#648E93",
+    accentInfoBackdrop: "#234B5C66",
+    accentWarn: "#F9B24F",
+    accentWarnBackdrop: "#89463266",
+    accentUrgent: "#F2257E",
+    accentUrgentBackdrop: "#76378B66",
+    accentGood: "#B5C558",
+    accentGoodBackdrop: "#46715A66",
+}
+
+export default defaultColors;
