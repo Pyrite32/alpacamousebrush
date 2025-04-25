ab49ae02bb3881510afd71f185dd32c4c092e714
Pyrite32 <patrick@method93.com>
2025-04-25 11:38:47 -0700

    feat(client): prototype navbar

--- clientside/src/app/page.tsx
+++ clientside/src/app/page.tsx
@@ -1,10 +1,7 @@
-import GitHubIcon from "../../src/priscilla/icons/16/pixel-fill/GithubIcon";
-
 export default function Page() {
   return (
     <> 
       <h1>Main Page</h1>
-      <GitHubIcon color="#fefe20"></GitHubIcon>
     </>
   );
 }
